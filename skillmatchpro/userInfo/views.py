b72cafbe12522909f9242c3357347950b7974c09
Komal12 <upadhyay.ko@northeastern.edu>
2023-10-16 14:32:25 -0700

    Merge changes to settings.py

--- skillmatchpro/userInfo/views.py
+++ skillmatchpro/userInfo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import UserInfo
 
-# Create your views here.
+def testmysql(request):
+    users = UserInfo.objects.all()
+    if users:
+        user = users[0]
+        context = {
+            'user_id': user.userID,
+            'user_name': user.name,
+        }
+    else:
+        context = {
+            'user_id': "1001",
+            'user_name': "Group 4 Test User",
+        }
+    return render(request, 'home.html', context)

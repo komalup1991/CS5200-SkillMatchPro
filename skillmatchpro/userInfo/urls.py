ad2e4a2e3a9b8fb5dc361ec1d539e8ffd2e769c5
sylvia <sylvia@gmail.com>
2023-11-27 12:47:46 -0800

    update uerInfo url

--- skillmatchpro/userInfo/urls.py
+++ skillmatchpro/userInfo/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views # authentication view
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("", Index.as_view(), name="index"),
+    path("", LoginView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", auth_views.LogoutView.as_view(template_name='userInfo/logout.html'), name="logout"),

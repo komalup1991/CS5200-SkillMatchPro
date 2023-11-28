d6c933403b238c57214f5f0dac6df2e1e15f9c80
Komal12 <upadhyay.ko@northeastern.edu>
2023-11-27 16:14:51 -0800

    add functionality to raise dispute from project detail

--- skillmatchpro/dispute/forms.py
+++ skillmatchpro/dispute/forms.py
@@ -5,3 +5,13 @@ class DisputeForm(forms.ModelForm):
     class Meta:
         model = Dispute
         fields = ['type', 'content']
+
+    def __init__(self, *args, **kwargs):
+        super(DisputeForm, self).__init__(*args, **kwargs)
+        default_type = 'payment_issues'
+        default_content = 'Enter your issues here...'
+
+        self.initial['type'] = default_type
+        self.initial['content'] = default_content
+        self.fields['content'].widget = forms.Textarea(attrs={'rows': 4, 'cols': 25})
+

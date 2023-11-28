b75ce43568efe321e8b30489b929c3cad3d83a88
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-28 14:08:11 -0800

    add icon as default profile pic and fix bug of pic

--- skillmatchpro/utils/context_processors.py
+++ skillmatchpro/utils/context_processors.py
@@ -8,14 +8,12 @@ def getUserInfo(request):
             "user": {0: 'nomral', 1: "profile/img/1ac7bac1-f230-4b39-a392-91f5b53e0970.jpeg"}
         }
     cursor = connection.cursor()
-    cursor.execute('''SELECT type, profilePicture
-                   FROM UserInfo u
-                   JOIN Profile p
-                   on u.userID = p.userID
-                   WHERE u.userID = %s
+    cursor.execute('''SELECT profileType, profilePicture
+                   FROM Profile
+                   WHERE userID = %s
                    ''', (id))
-    user = cursor.fetchone
+    user = cursor.fetchone()
 
     return {
-        "user": user,
+        "user": user
     }

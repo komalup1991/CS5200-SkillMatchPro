74e6f156e4d2cef8e3c43a3cbbd646f830f9c7bd
sylvia <sylvia@gmail.com>
2023-11-27 17:10:51 -0800

    upload profile pic

--- skillmatchpro/userInfo/forms.py
+++ skillmatchpro/userInfo/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 # inherits from the UserCreationForm
 class UserRegisterForm(UserCreationForm):
     # username and password already included
